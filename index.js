7706a1a26db0dc15f3ff79744ee4cd40535647e2
silverwind <me@silverwind.io>
2015-09-12 23:05:58 +0200

    remove optional parameter and model name

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ var crypto = require("crypto");
 var asn1 = require("asn1.js");
 
 module.exports = function dhparam(bits) {
-  return asn1.define("Model", function () {
-    this.seq().obj(
-      this.key("p").int(),
-      this.key("g").int(),
-      this.key("privateValueLength").optional().int()
-    );
+  return asn1.define("", function () {
+    this.seq().obj(this.key("p").int(), this.key("g").int());
   }).encode({
     p: crypto.createDiffieHellman(bits || 1024).getPrime(),
     g: 2,
   }, "pem", {label: "DH PARAMETERS"});
-}
+};
